5173d3638cc58bfacf563b1c77ef429efdebb801
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-02 20:23:04 +0200

    Work on wine table

--- src/features/wine/WineList.tsx
+++ src/features/wine/WineList.tsx
@@ -1,3 +1,6 @@
+import {
+  Table, Tbody, Th, Thead, Tr,
+} from '@chakra-ui/react';
 import React from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { selectWines } from '../../app/selectors';
@@ -5,9 +8,54 @@ import { selectWines } from '../../app/selectors';
 const WineList: React.FC = () => {
   const wines = useAppSelector(selectWines);
 
-  const renderWines = (): JSX.Element[] => wines.map(({ name }) => <p key={name}>{name}</p>);
+  const sortWines = (key: string): void => {
+    // How to sort wines e.g. by type or volume?
+    console.log(key);
+  };
 
-  return <div>{renderWines()}</div>;
+  const renderHeader = (header: string): JSX.Element => (
+    <Th
+      key={header}
+      onClick={() => sortWines(header)}
+      style={{ cursor: 'pointer' }}
+    >
+      {header}
+    </Th>
+  );
+
+  const renderTableHead = (): JSX.Element => {
+    const headers = ['Nimi', 'Tyyppi', 'Maa', 'Määrä (l)', 'Hinta (€)'];
+    return (
+      <Thead>
+        <Tr>
+          {headers.map(renderHeader)}
+        </Tr>
+      </Thead>
+    );
+  };
+
+  const renderTablebody = (): JSX.Element => (
+    <Tbody>
+      {wines.map(({
+        id, name, type, country, volume, price,
+      }) => (
+        <Tr key={id}>
+          <Th>{name}</Th>
+          <Th>{type}</Th>
+          <Th>{country}</Th>
+          <Th>{volume}</Th>
+          <Th>{price}</Th>
+        </Tr>
+      ))}
+    </Tbody>
+  );
+
+  return (
+    <Table variant="striped">
+      {renderTableHead()}
+      {renderTablebody()}
+    </Table>
+  );
 };
 
 export default WineList;
