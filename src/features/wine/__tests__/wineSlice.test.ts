b69d6c9bd4f47f395b36e18a12bed9b2735985dd
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-11 17:58:51 +0200

    Reducer tests

--- src/features/wine/__tests__/wineSlice.test.ts
+++ src/features/wine/__tests__/wineSlice.test.ts
@@ -1,4 +1,5 @@
-import wineReducer, { WineState } from '../wineSlice';
+import reducer, { sortAsc, sortDesc, WineState } from '../wineSlice';
+import { wines } from '../../../tests/testdata';
 
 describe('Wine reducer', () => {
   it('should handle initial state', () => {
@@ -6,6 +7,25 @@ describe('Wine reducer', () => {
       wines: [],
       status: 'idle',
     };
-    expect(wineReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+    expect(reducer(undefined, { type: 'unknown' })).toEqual(initialState);
+  });
+});
+
+describe('Sorting wines', () => {
+  const previousState: WineState = {
+    wines,
+    status: 'idle',
+  };
+
+  it('sortAsc sorts wines in ascending order', () => {
+    const state = reducer(previousState, sortAsc('country'));
+    const countries = state.wines.map((wine) => wine.country);
+    expect(countries).toEqual(['Italy', 'Spain']);
+  });
+
+  it('sortDesc sorts wines in descending order', () => {
+    const state = reducer(previousState, sortDesc('volume'));
+    const volumes = state.wines.map((wine) => wine.volume);
+    expect(volumes).toEqual([3, 0.75]);
   });
 });
