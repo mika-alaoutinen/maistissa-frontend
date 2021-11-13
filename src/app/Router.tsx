00bcc98074741194a788dc51ffb7006b0eacb89a
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-13 11:39:09 +0200

    Add Router

---
+++ src/app/Router.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../views/HomePage';
+import ReviewPage from '../views/ReviewPage';
+import WinePage from '../views/WinePage';
+
+interface Link {
+  text: string
+  to: string
+}
+
+const home: Link = { text: 'Home', to: '/' };
+const reviews: Link = { text: 'Reviews', to: '/reviews' };
+const wines: Link = { text: 'Wines', to: '/wines' };
+
+const Router: React.FC = () => (
+  <Routes>
+    <Route path={home.to} element={<HomePage />} />
+    <Route path={reviews.to} element={<ReviewPage />} />
+    <Route path={wines.to} element={<WinePage />} />
+  </Routes>
+);
+
+export const links = [home, reviews, wines];
+export default Router;
