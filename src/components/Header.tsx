d0aa4fa56a455d3cd58075a94246807e689b4db7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-13 11:47:45 +0200

    Add home link to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
+import { Link } from '@chakra-ui/react';
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { links } from '../app/Router';
 
-const Header: React.FC = () => (
-  <header className="header">
-    <h1>Maistissa</h1>
-  </header>
-);
+const Header: React.FC = () => {
+  const [home] = links;
+
+  return (
+    <header className="header">
+      <Link as={RouterLink} to={home.to}>Maistissa</Link>
+    </header>
+  );
+};
 
 export default Header;
