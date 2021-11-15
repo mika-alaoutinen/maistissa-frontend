3580dbeffecd3a95a88fd069015f7c6d6649a625
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-15 18:45:44 +0200

    Rename WineList to Wines

--- src/features/wine/__tests__/WineList.test.tsx
+++ src/features/wine/__tests__/WineList.test.tsx
@@ -1,13 +1,13 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
 import { renderWithStore } from '../../../tests/testutils';
-import WineList from '../WineList';
+import Wines from '../Wines';
 
 // No idea why Eslint warns about any type here
 describe('Displays wine names', () => {
   it('renders two wine names as paragraphs', () => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    renderWithStore(<WineList />);
+    renderWithStore(<Wines />);
     expect(screen.getAllByText(/wine/)).toHaveLength(2);
   });
 });
