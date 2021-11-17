7e7bd0cc01aed743bb79be731ddec2dd54ba7cc8
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-17 18:13:38 +0200

    Improve reviewSlice tests

--- src/features/review/__tests__/reviewSlice.test.ts
+++ src/features/review/__tests__/reviewSlice.test.ts
@@ -1,4 +1,5 @@
-import reviewReducer, { ReviewState } from '../reviewSlice';
+import reviewReducer, { ReviewState, sortAsc, sortDesc } from '../reviewSlice';
+import { reviews } from '../../../tests/testdata';
 
 describe('Review reducer', () => {
   it('should handle initial state', () => {
@@ -7,5 +8,24 @@ describe('Review reducer', () => {
       status: 'idle',
     };
     expect(reviewReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+  });
+});
+
+describe('Sorting reviews', () => {
+  const previousState: ReviewState = {
+    reviews,
+    status: 'idle',
+  };
+
+  it('sortAsc sorts reviews in ascending order', () => {
+    const state = reviewReducer(previousState, sortAsc('author'));
+    const authors = state.reviews.map((review) => review.author);
+    expect(authors).toEqual(['Kukko Pena', 'Pekka Kana']);
+  });
+
+  it('sortDesc sorts reviews in descending order', () => {
+    const state = reviewReducer(previousState, sortDesc('date'));
+    const dates = state.reviews.map((review) => review.date);
+    expect(dates).toEqual(['2020-01-02', '2020-01-01']);
   });
 });
