6bff4eb49452810076f41d2774c0d186a6f4ad41
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-20 20:06:46 +0200

    Add generic table to HomePage

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { useAppSelector } from '../app/hooks';
+import { selectWines, selectWinesSorted } from '../app/selectors';
+import DataTable from '../components/datatable/DataTable';
+import { useSorting } from '../features/wine/hooks/wineSorting';
+import { WineProps } from '../features/wine/wineAPI';
 
-const HomePage: React.FC = () => (
-  <div>
-    <h1>Home page</h1>
-  </div>
-);
+type Headers = {
+  key: WineProps,
+  text: string
+};
+
+const wineHeaders: Headers[] = [
+  {
+    key: 'name',
+    text: 'Name',
+  },
+  {
+    key: 'type',
+    text: 'Type',
+  },
+  {
+    key: 'country',
+    text: 'Country',
+  },
+  {
+    key: 'volume',
+    text: 'Volume (l)',
+  },
+  {
+    key: 'price',
+    text: 'Price (€)',
+  },
+];
+
+const HomePage: React.FC = () => {
+  const wines = useAppSelector(selectWines).map(({
+    name, type, country, volume, price,
+  }) => ({
+    name, type, country, volume, price,
+  }));
+
+  const sortingFn = useSorting();
+
+  return (
+    <div>
+      <h1>Home page</h1>
+      <DataTable
+        data={wines}
+        headers={wineHeaders}
+        selector={selectWinesSorted}
+        sortingFn={sortingFn}
+      />
+    </div>
+  );
+};
 
 export default HomePage;
