f4655e94d31497748930d231fad4ec9180995cb2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-21 14:05:35 +0200

    Make sorted and sortingFn optional

--- src/components/datatable/DataTable.tsx
+++ src/components/datatable/DataTable.tsx
@@ -7,20 +7,27 @@ import { Column, SortedByKey } from './types';
 interface Props<T, K extends keyof T> {
   columns: Column<T, K>[];
   data: T[];
-  sorted: SortedByKey<T, K>;
-  sortingFn: (key: K) => void;
+  sorted?: SortedByKey<T, K>;
+  sortingFn?: (key: K) => void;
 }
 
 const DataTable = <T, K extends keyof T>({
   columns,
   data,
-  sorted,
-  sortingFn,
+  sorted = { direction: 'unsorted' },
+  sortingFn = () => {},
 }: Props<T, K>): JSX.Element => (
   <Table variant="striped">
     <DataTableHead columns={columns} sorted={sorted} sortingFn={sortingFn} />
     <DataTableBody columns={columns} data={data} />
   </Table>
   );
+
+DataTable.defaultProps = {
+  sorted: {
+    direction: 'unknown',
+  },
+  sortingFn: () => {},
+};
 
 export default DataTable;
