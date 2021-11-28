7dc47d66084cc72fdf886314eefdeabf536dcf2b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-28 16:06:39 +0200

    Tests for selectors

--- src/app/__tests__/selectors.test.ts
+++ src/app/__tests__/selectors.test.ts
@@ -1,7 +1,13 @@
 import { initRootState } from '../../tests/testutils';
-import { selectReviews, selectWines } from '../selectors';
+import {
+  selectReviews,
+  selectReviewsSorted,
+  selectWineCountries,
+  selectWines,
+  selectWinesSorted,
+} from '../selectors';
 
-describe('Selectors', () => {
+describe('Wine selectors', () => {
   const state = initRootState();
 
   it('should return all wines', () => {
@@ -10,9 +16,34 @@ describe('Selectors', () => {
     expect(wines[1].name).toBe('Red wine 1');
   });
 
+  it('should return wines\' sorting info', () => {
+    const winesSorted = selectWinesSorted(state);
+    expect(winesSorted.direction).toBe('unsorted');
+    expect(winesSorted.key).toBeFalsy();
+  });
+});
+
+describe('Review selectors', () => {
+  const state = initRootState();
+
   it('should return all reviews', () => {
     const reviews = selectReviews(state);
     expect(reviews[0].author).toBe('Pekka Kana');
     expect(reviews[1].author).toBe('Kukko Pena');
+  });
+
+  it('should return reviews\' sorting info', () => {
+    const reviewsSorted = selectReviewsSorted(state);
+    expect(reviewsSorted.direction).toBe('unsorted');
+    expect(reviewsSorted.key).toBeFalsy();
+  });
+});
+
+describe('Wine info selectors', () => {
+  const state = initRootState();
+
+  it('should return wine countries', () => {
+    const countries = selectWineCountries(state);
+    expect(countries).toEqual(['Italy', 'Spain']);
   });
 });
