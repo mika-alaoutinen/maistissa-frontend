64d8a97820b9c1ad36f74a90616305da5980f661
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-11-29 14:22:08 +0200

    Fix broken test

--- src/views/__tests__/AddWinePage.test.tsx
+++ src/views/__tests__/AddWinePage.test.tsx
@@ -11,13 +11,16 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe('Fetches wine countries in component load', () => {
-  const countriesResponse = ['Italy', 'Spain'];
+describe('Fetches wine info on component load', () => {
+  it('should dispatch three thunks', async () => {
+    mockAPI.getCountries.mockResolvedValue(Promise.resolve(['Italy', 'Spain']));
+    mockAPI.getDescriptions.mockResolvedValue(Promise.resolve(['dry', 'aromatic', 'full bodied', 'rich']));
+    mockAPI.getFoodPairings.mockResolvedValue(Promise.resolve(['white meat', 'barbeque']));
 
-  it('should dispatch a fetchCountries thunk', async () => {
-    mockAPI.getCountries.mockResolvedValue(Promise.resolve(countriesResponse));
     renderWithStore(<AddWinePage />);
     expect(mockAPI.getCountries).toHaveBeenCalledTimes(1);
+    expect(mockAPI.getDescriptions).toHaveBeenCalledTimes(1);
+    expect(mockAPI.getFoodPairings).toHaveBeenCalledTimes(1);
     expect(await screen.findByText('Add new wine')).toBeInTheDocument();
   });
 });
