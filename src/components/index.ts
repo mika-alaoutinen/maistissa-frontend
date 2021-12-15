1a329540654401b1ed3c82a2719af8cebc37867d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-12-15 09:06:54 +0200

    Move Combobox component to its own folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 import DataTable from './datatable/DataTable';
-import Combobox from './input/Combobox';
+import Combobox from './combobox/Combobox';
 import Input from './input/Input';
 import NumberInput from './input/NumberInput';
 
