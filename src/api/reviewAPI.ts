2f547e98281b3ae34de0751d048bc5f5430db72d
Mika Alaoutinen <42463672+mika-alaoutinen@users.noreply.github.com>
2021-12-29 18:12:01 +0200

    Npm major update (#21)
    Update all packages to latest versions

--- src/api/reviewAPI.ts
+++ src/api/reviewAPI.ts
@@ -1,8 +1,15 @@
 import { NewReview } from '../generated/models/NewReview';
 import { Review } from '../generated/models/Review';
 import { ReviewsCrudService } from '../generated/services/ReviewsCrudService';
+import { createError } from './errorHandling';
 
-const getReviews = async (): Promise<Review[]> => ReviewsCrudService.getReviews();
+const getReviews = async (): Promise<Review[]> => {
+  try {
+    return await ReviewsCrudService.getReviews();
+  } catch (error) {
+    throw createError(error);
+  }
+};
 
 export type { NewReview, Review };
 export default { getReviews };
