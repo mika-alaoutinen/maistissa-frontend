6bf551aefaac5fedff4653a56bc50ae33f9538cf
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-01-07 08:59:31 +0200

    Change ValidationError to have arrays of error messages as keys

--- src/features/form/__tests__/validation.test.ts
+++ src/features/form/__tests__/validation.test.ts
@@ -1,15 +1,18 @@
-import { ValidationRules, validate } from '../validation';
+import { ValidationRules, initErrors, validate } from '../validation';
 
 interface Data {
   value?: string;
 }
+
+const REQUIRED_ERROR_MESSAGE = 'value is required';
+const CUSTOM_ERROR_MESSAGE = 'string should be > 3 characters';
 
 describe('Validation should handle required field', () => {
   const rulesRequired: ValidationRules<Data> = {
     value: {
       required: {
         value: true,
-        message: 'value is required',
+        message: REQUIRED_ERROR_MESSAGE,
       },
     },
   };
@@ -25,22 +28,22 @@ describe('Validation should handle required field', () => {
 
   it('should be error when required field is undefined', () => {
     const errors = validate({ value: undefined }, rulesRequired);
-    expect(errors?.value).toBe('value is required');
+    expect(errors?.value).toEqual([REQUIRED_ERROR_MESSAGE]);
   });
 
   it('should not be error when required field has value', () => {
     const errors = validate({ value: 'foo' }, rulesRequired);
-    expect(errors?.value).toBeUndefined();
+    expect(errors?.value).toEqual([]);
   });
 
   it('should not be error when optional field is undefined', () => {
     const errors = validate({ value: undefined }, rulesOptional);
-    expect(errors?.value).toBeUndefined();
+    expect(errors?.value).toEqual([]);
   });
 
   it('should not be error when optional field has value', () => {
     const errors = validate({ value: 'foo' }, rulesOptional);
-    expect(errors?.value).toBeUndefined();
+    expect(errors?.value).toEqual([]);
   });
 });
 
@@ -48,25 +51,25 @@ describe('Validation should handle custom validation rule', () => {
   const customRules: ValidationRules<Data> = {
     value: {
       valid: {
-        isValid: (s: string | undefined) => (s ? s.length > 3 : false),
-        message: 'string should be > 3 characters',
+        func: (s: string | undefined) => (s ? s.length > 3 : false),
+        message: CUSTOM_ERROR_MESSAGE,
       },
     },
   };
 
   it('should be error when invalid', () => {
     const errors = validate({ value: '123' }, customRules);
-    expect(errors?.value).toBe('string should be > 3 characters');
+    expect(errors?.value).toEqual([CUSTOM_ERROR_MESSAGE]);
   });
 
   it('should be error when undefined is passed to the validation function', () => {
     const errors = validate({ value: undefined }, customRules);
-    expect(errors?.value).toBe('string should be > 3 characters');
+    expect(errors?.value).toEqual([CUSTOM_ERROR_MESSAGE]);
   });
 
   it('should not be error when valid', () => {
     const errors = validate({ value: '1234' }, customRules);
-    expect(errors?.value).toBeUndefined();
+    expect(errors?.value).toEqual([]);
   });
 });
 
@@ -75,17 +78,29 @@ describe('Should return both required and custom errors', () => {
     value: {
       required: {
         value: true,
-        message: 'value is required',
+        message: REQUIRED_ERROR_MESSAGE,
       },
       valid: {
-        isValid: (s: string | undefined) => (s ? s.length > 3 : false),
-        message: 'string should be > 3 characters',
+        func: (s: string | undefined) => (s ? s.length > 3 : false),
+        message: CUSTOM_ERROR_MESSAGE,
       },
     },
   };
 
-  it('should return required error when both types of errors are found', () => {
+  it('should return both types of errors', () => {
     const errors = validate({ value: undefined }, rules);
-    expect(errors?.value).toBe('value is required');
+    expect(errors?.value).toEqual([REQUIRED_ERROR_MESSAGE, CUSTOM_ERROR_MESSAGE]);
+  });
+});
+
+describe('Should initialize an empty error object with correct keys and empty arrays as values', () => {
+  const emptyErrors = initErrors({ value: 'foo' });
+
+  it('should have all keys of target type', () => {
+    expect(emptyErrors).toHaveProperty('value');
+  });
+
+  it('should have empty array as value', () => {
+    expect(emptyErrors.value).toEqual([]);
   });
 });
