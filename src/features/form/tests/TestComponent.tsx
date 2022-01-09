0fa69516d16081da21761f67fefeb32616f258f1
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2022-01-09 11:42:57 +0200

    Add tests for useForm

--- src/features/form/tests/TestComponent.tsx
+++ src/features/form/tests/TestComponent.tsx
@@ -5,6 +5,7 @@ import { ValidationRules } from '../validation';
 interface Props {
   initialData: Data;
   rules?: ValidationRules<Data>;
+  onSubmit?: () => void;
 }
 
 export interface Data {
@@ -15,12 +16,12 @@ export interface Data {
  * A component for testing the useForm hook.
  * @returns component that uses the useForm hook.
  */
-const TestComponent: React.FC<Props> = ({ initialData, rules }) => {
+const TestComponent: React.FC<Props> = ({ initialData, rules, onSubmit }) => {
   const { data, errors, onChange } = useForm<Data>(initialData, rules);
 
   const errorMessages: JSX.Element = (
     <div>
-      {errors.value.map((e) => <div>{e}</div>)}
+      {errors.value.map((e) => <div key={e}>{e}</div>)}
     </div>
   );
 
@@ -40,6 +41,10 @@ const TestComponent: React.FC<Props> = ({ initialData, rules }) => {
       >
         {`value: ${data.value}`}
       </label>
+
+      <button onClick={onSubmit} type="submit">
+        Submit
+      </button>
     </div>
   );
 };
--- src/features/form/tests/useForm.test.tsx
+++ src/features/form/tests/useForm.test.tsx
@@ -5,13 +5,19 @@ import userEvent from '@testing-library/user-event';
 import TestComponent, { Data } from './TestComponent';
 import { ValidationRules } from '../validation';
 
-describe('Displaying data and errors', () => {
+const typeIntoInput = (text: string): void => {
+  userEvent.type(screen.getByTestId('test-input'), text);
+};
+
+describe('Data is displayed', () => {
   it('data is displayed', () => {
     render(<TestComponent initialData={{ value: 'initial' }} />);
     expect(screen.getByText(/initial/)).toBeInTheDocument();
   });
+});
 
-  it('errors are displayed', () => {
+describe('Errors are displayed based on given validation rules', () => {
+  it('error is displayed when required field is empty', () => {
     const rules: ValidationRules<Data> = {
       mode: 'ON_CHANGE',
       validations: {
@@ -23,19 +29,58 @@ describe('Displaying data and errors', () => {
         },
       },
     };
-    render(<TestComponent initialData={{ value: 'initial' }} rules={rules} />);
-    expect(screen.getByText(/initial/)).toBeInTheDocument();
+
+    render(<TestComponent initialData={{ value: '' }} rules={rules} />);
+    expect(screen.queryByText(/required/)).not.toBeInTheDocument();
+
+    typeIntoInput('foo');
+    userEvent.clear(screen.getByTestId('test-input'));
+    expect(screen.getByText(/required/)).toBeInTheDocument();
+  });
+
+  it('error is displayed when custom validation function fails', () => {
+    const rules: ValidationRules<Data> = {
+      mode: 'ON_CHANGE',
+      validations: {
+        value: {
+          valid: {
+            func: (value) => value.length < 2,
+            message: 'validation failed',
+          },
+        },
+      },
+    };
+
+    render(<TestComponent initialData={{ value: '' }} rules={rules} />);
+
+    typeIntoInput('f');
+    expect(screen.queryByText(/failed/)).not.toBeInTheDocument();
+
+    typeIntoInput('o');
+    expect(screen.getByText(/failed/)).toBeInTheDocument();
   });
 });
 
 describe('OnChange updates the hooks internal state', () => {
-  beforeEach(() => render(<TestComponent initialData={{ value: '' }} />));
-
   it('onChange updates data', () => {
+    render(<TestComponent initialData={{ value: '' }} />);
     expect(screen.queryByText(/foo/)).not.toBeInTheDocument();
-    userEvent.type(screen.getByTestId('test-input'), 'foo');
+
+    typeIntoInput('foo');
     expect(screen.getByText(/foo/)).toBeInTheDocument();
   });
 });
 
-export default TestComponent;
+describe('OnSubmit is used to validate and submit the form', () => {
+  const clickSubmit = (): void => {
+    userEvent.click(screen.getByText('Submit'));
+  };
+
+  it('onSubmit calls submit handler function that is given as parameter', () => {
+    const onSubmit = jest.fn();
+    render(<TestComponent initialData={{ value: '' }} onSubmit={onSubmit} />);
+
+    clickSubmit();
+    expect(onSubmit).toHaveBeenCalledTimes(1);
+  });
+});
